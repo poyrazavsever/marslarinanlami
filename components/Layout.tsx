3c6612e9f8ccacd135288f8d1089a1f53e6f187a
poyrazavsever <poyrazavsever@hotmail.com>
2025-05-04 11:52:57 +0300

    feat: created layout

---
+++ components/Layout.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col">
+
+      <Navbar />
+
+      <main className="flex-grow">{children}</main>
+
+      <Footer />
+      
+    </div>
+  );
+};
+
+export default Layout;
