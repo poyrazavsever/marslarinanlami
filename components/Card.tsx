7f1d76fe5534de1a1d462624292e1fe11974eb3c
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-12 10:27:24 +0300

    feat: updated card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -14,7 +14,10 @@ const Card: React.FC<CardProps> = ({ id, title, mars }) => {
       className="block border border-neutral-300 rounded-md p-4 hover:shadow-sm hover:border-neutral-400 transition bg-white cursor-pointer"
     >
       <h3 className="font-medium text-lg text-neutral-800 mb-2">{title}</h3>
-      <p className="text-neutral-600 text-sm line-clamp-2">{mars}</p>
+      <div
+        className="text-neutral-600 text-sm line-clamp-4"
+        dangerouslySetInnerHTML={{ __html: mars }}
+      />
     </Link>
   );
 };
